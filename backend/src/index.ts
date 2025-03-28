38abca723bc24cf9bc1dd705d87adead9029f2ad
koushik bharati <koushikbharati7@gmail.com>
2025-03-29 01:15:37 +0530

    export app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,5 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+export default app;
